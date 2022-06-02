d11cf95e941755c61f182f6c3494f562307786d3
marius.crisan <marius.crisan.tech@gmail.com>
2022-06-02 11:16:42 +0300

    Final

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -87,7 +87,7 @@ class RecipeImageSerializer(serializers.ModelSerializer):
     """Serializer for uploading images to recipes"""
 
     class Meta:
-        model=Recipe
+        model = Recipe
         fields = ['id', 'image']
         read_only_fields = ['id']
         extra_kwargs = {'image': {'required': 'True'}}
